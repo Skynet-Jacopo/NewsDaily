477e5bc6daa3fe0cd6828864d9a48e1fea7732e9
Skynet-Jacopo <905169916@qq.com>
2016-05-30 17:44:48 +0800

    修改了类名并新添加了是否第一次运行(是第一次运行?LeadActivity:SplashActivity) 并添加了官方文档里给的两个动画

--- app/src/main/java/com/example/liuqun/newsdaily/ui/SplashActivity.java
+++ app/src/main/java/com/example/liuqun/newsdaily/ui/SplashActivity.java
@@ -1,132 +1,15 @@
 package com.example.liuqun.newsdaily.ui;
 
-import android.annotation.TargetApi;
-import android.content.Intent;
-import android.os.Build;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.example.liuqun.newsdaily.R;
 
-import java.util.ArrayList;
-
-public class SplashActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
-
-    private ViewPager       pager;
-    private ArrayList<View> list;
-
-    int[] pics = {R.drawable.welcome, R.drawable.small, R.drawable.bd, R.drawable
-            .wy};
-    //四个点
-    private ImageView[] points = new ImageView[4];
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        //初始化4个点
-        initPoints();
-        //初始化ViewPager
-        initView();
-    }
-
-    private void initPoints() {
-        points[0] = (ImageView) findViewById(R.id.iv_p1);
-        points[1] = (ImageView) findViewById(R.id.iv_p2);
-        points[2] = (ImageView) findViewById(R.id.iv_p3);
-        points[3] = (ImageView) findViewById(R.id.iv_p4);
-        setPoint(0);
-    }
-
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    private void setPoint(int index) {
-        for (int i = 0; i < points.length; i++) {
-            if (i == index) {
-                points[i].setImageAlpha(255);
-            } else {
-                points[i].setImageAlpha(100);
-            }
-        }
-    }
-
-    private void initView() {
-        list = new ArrayList<>();
-
-        pager = (ViewPager) findViewById(R.id.vp_guide);
-
-        for (int i = 0; i < pics.length; i++) {
-            ImageView iv = new ImageView(this);
-            iv.setScaleType(ImageView.ScaleType.FIT_XY);
-            iv.setImageResource(pics[i]);
-            list.add(iv);
-        }
-        pager.setAdapter(new MyPagerAdapter(list));
-        pager.addOnPageChangeListener(this);
-
-    }
-
-    //界面切换时调用
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-    }
-
-    //界面切换后调用
-    @Override
-    public void onPageSelected(int position) {
-        setPoint(position);
-        if (position >= 3) {
-            Intent intent = new Intent(SplashActivity.this, LogoActivity.class);
-            startActivity(intent);
-            finish();
-        }
-
-    }
-
-    //滑动状态变化时调用
-    @Override
-    public void onPageScrollStateChanged(int state) {
-
-    }
-
-    //自定义MyPagerAdapter继承PagerAdapter
-    private class MyPagerAdapter extends PagerAdapter {
-
-        private ArrayList<View> list;
-
-        public MyPagerAdapter(ArrayList<View> list) {
-            this.list = list;
-        }
-
-        //初始化position 展现到界面上来
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            container.addView(list.get(position), 0);
-            return list.get(position);
-        }
-
-        //当不可见时,销毁position
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView(list.get(position));
-        }
-
-        @Override
-        public int getCount() {
-            if (list != null) {
-                return list.size();
-            }
-            return 0;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
-        }
+        setContentView(R.layout.activity_logo);
     }
 }
