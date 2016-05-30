0d10454cbcc4dcfd07641e3e99721cc5da488d54
Skynet-Jacopo <905169916@qq.com>
2016-05-30 17:19:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/liuqun/newsdaily/ui/LogoActivity.java
@@ -0,0 +1,15 @@
+package com.example.liuqun.newsdaily.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.liuqun.newsdaily.R;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}
